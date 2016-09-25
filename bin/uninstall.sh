2670dc9fc8c137ee904b62416a7e451ebcc910f0
Flaviene Scheidt De Cristo <fsc10@inf.ufpr.br>
2016-09-25 12:05:15 -0300

    Changes at the uninstall. Some procedures where not being deleted

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -104,4 +104,104 @@ else
 	echo "[OK] Functions destroyed"
 fi
 
+sql="DROP FUNCTION return_energy_by_job(varchar)" 
+psql -c "$sql" ${DB_NAME} ${DB_USER}
+
+if [ $? -ne 0 ]
+then
+	echo "[ERROR] Could not destroy function"
+else
+	echo "[OK] Functions destroyed"
+fi
+
+sql="DROP FUNCTION return_energy_by_code()" 
+psql -c "$sql" ${DB_NAME} ${DB_USER}
+
+if [ $? -ne 0 ]
+then
+	echo "[ERROR] Could not destroy function"
+else
+	echo "[OK] Functions destroyed"
+fi
+
+sql="DROP FUNCTION return_energy_ranking_job(int)" 
+psql -c "$sql" ${DB_NAME} ${DB_USER}
+
+if [ $? -ne 0 ]
+then
+	echo "[ERROR] Could not destroy function"
+else
+	echo "[OK] Functions destroyed"
+fi
+
+sql="DROP FUNCTION return_code_sign_by_query(varchar)" 
+psql -c "$sql" ${DB_NAME} ${DB_USER}
+
+if [ $? -ne 0 ]
+then
+	echo "[ERROR] Could not destroy function"
+else
+	echo "[OK] Functions destroyed"
+fi
+
+sql="DROP FUNCTION return_query_job_by_sign(int)" 
+psql -c "$sql" ${DB_NAME} ${DB_USER}
+
+if [ $? -ne 0 ]
+then
+	echo "[ERROR] Could not destroy function"
+else
+	echo "[OK] Functions destroyed"
+fi
+
+sql="DROP FUNCTION return_energy_ranking_by_size(bigint)" 
+psql -c "$sql" ${DB_NAME} ${DB_USER}
+
+if [ $? -ne 0 ]
+then
+	echo "[ERROR] Could not destroy function"
+else
+	echo "[OK] Functions destroyed"
+fi
+
+sql="DROP FUNCTION return_plots_sum (varchar)" 
+psql -c "$sql" ${DB_NAME} ${DB_USER}
+
+if [ $? -ne 0 ]
+then
+	echo "[ERROR] Could not destroy function"
+else
+	echo "[OK] Functions destroyed"
+fi
+
+sql="DROP FUNCTION return_energy_ranking_job_sum (int)" 
+psql -c "$sql" ${DB_NAME} ${DB_USER}
+
+if [ $? -ne 0 ]
+then
+	echo "[ERROR] Could not destroy function"
+else
+	echo "[OK] Functions destroyed"
+fi
+
+sql="DROP FUNCTION return_energy_ranking_by_size_sum (bigint)" 
+psql -c "$sql" ${DB_NAME} ${DB_USER}
+
+if [ $? -ne 0 ]
+then
+	echo "[ERROR] Could not destroy function"
+else
+	echo "[OK] Functions destroyed"
+fi
+
+sql="DROP FUNCTION return_energy_ranking_sum ()" 
+psql -c "$sql" ${DB_NAME} ${DB_USER}
+
+if [ $? -ne 0 ]
+then
+	echo "[ERROR] Could not destroy function"
+else
+	echo "[OK] Functions destroyed"
+fi
+
 #Empty salaak web interface
